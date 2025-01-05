66a4bf98ee81b1c9872d4ce08b77a4001656029e
Ronak99 <punase.ronak99@gmail.com>
2025-01-06 00:23:07 +0530

    Added real URL

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,5 +1,5 @@
-export const API_BASE_URL = "http://localhost:3000";
-// export const API_BASE_URL = "https://inquirely-web.vercel.app";
+// export const API_BASE_URL = "http://localhost:3000";
+export const API_BASE_URL = "https://inquirely-web.vercel.app";
 
 export const API_ENDPOINTS = {
   ASSISTANTS: "/api/assistants",
